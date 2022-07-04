998075145362d0286151ebd986b52abf7ea6b57e
dududayo <dududayo37@gmail.com>
2022-07-04 18:46:18 +0900

    999

--- GitBackProject2/src/com/sist/main/MainClass.java
+++ GitBackProject2/src/com/sist/main/MainClass.java
@@ -5,7 +5,6 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git-1");
-		System.out.println("Hello Git-2");
 	}
 
 }
